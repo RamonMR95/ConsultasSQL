3eb339180b9c502fe1b1767c10ff28a1226a85f7
Ramon Moñino <ramonmr16@gmail.com>
2019-05-28 21:44:44 +0200

    ADD Enunciados de Procedimientos con cursores

--- Cursors/cursores.sql
+++ Cursors/cursores.sql
@@ -1,4 +1,10 @@
-/* EJERCICIO 1 */
+/* EJERCICIO 1
+Crear un procedimiento que incremente de forma condicionada el stock de todos los artículos en una cantidad dada de forma que:
+se pase como parámetro la cantidad a sumar
+si el stock actual es <= 10 sumarle la cantidad pasada
+si el stock actual es > 10 y <= 100 no sumarle nada
+si el stock actual es > 100 sumarle el doble de la cantidad pasada como parámetro
+*/
 DELIMITER //
 CREATE PROCEDURE ej1(IN v_cantidad INT)
 BEGIN
@@ -34,7 +40,13 @@ END //
 DELIMITER ;
 
 
-/* EJERCICIO 2 */
+/* EJERCICIO 2
+Elaborar un procedimiento con cursores sobre la BD facturacion que dado un stock_minimo
+(pasado como parámetro al procedimiento) compruebe si el stock de cada artículo está por
+encima o por debajo y muestre, para los que están por debajo del valor mínimo, el número
+de unidades que se deben pedir de cada artículo para estar 10 unidades por encima del
+stock mínimo. Si el stock es igual o mayor que el stock mínimo debe mostrar un NULL.
+*/
 DELIMITER //
 CREATE PROCEDURE ej2(IN stock_minimo INT)
 BEGIN
@@ -68,7 +80,11 @@ BEGIN
 DELIMITER ;
 
 
-/* EJERCICIO 3 */
+/* EJERCICIO 3
+Elaborar un procedimiento con cursores que compruebe los clientes mayores de
+60 años y los menores de 30 para mostrar el rango de edad de cada uno en cada
+caso. Porejemplo: jóvenes, adultos, jubilados
+*/
 DELIMITER //
 CREATE PROCEDURE ej3()
 BEGIN
@@ -108,7 +124,12 @@ END //
 DELIMITER ;
 
 
-/* EJERCICIO 4 */
+/* EJERCICIO 4
+Elaborar un procedimiento con cursores que actualice el campo dirección de los
+clientes con motivo del cambio de dos calles:  'Carril La Iglesia' ha pasado
+a llamarse 'C/ La Iglesia' y la 'C/ Almudena' ha pasado a llamarse
+'Avda Mediterráneo'
+*/
 DELIMITER //
 CREATE PROCEDURE ej4()
 BEGIN
@@ -140,13 +161,3 @@ BEGIN
   CLOSE cliente_direccion;
 END //
 DELIMITER ;
-
-
-/*
-1.- create procedure ej1(IN v_cantidad INT) BEGIN DECLARE v_stock int(10); DECLARE v_cod VARCHAR(8); DECLARE fin INT DEFAULT 0; DECLARE c_stock CURSOR FOR SELECT cod_art,stock FROM articulo; DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin=1; OPEN c_stock; get_stock:LOOP FETCH c_stock INTO v_cod,v_stock; IF fin=1 THEN  LEAVE get_stock; END IF; IF v_stock<=10 THEN UPDATE articulo SET stock=v_stock+v_cantidad WHERE cod_art=v_cod; ELSEIF v_stock>100 THEN UPDATE articulo SET stock=v_stock+v_cantidad*2 WHERE cod_art=v_cod;  END IF; END LOOP get_stock; CLOSE c_stock; END//
-2.- CREATE PROCEDURE ej2(stock_minimo integer) BEGIN DECLARE v_stock INT; declare v_cod VARCHAR(30); declare pedir int; DECLARE fin INTEGER DEFAULT 0; DECLARE stock_art CURSOR FOR   SELECT stock,cod_art FROM articulo; DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin=1; OPEN stock_art; get_arts: LOOP   FETCH stock_art INTO v_stock,v_cod ; IF fin=1 THEN LEAVE get_arts; END IF;  IF v_stock < stock_minimo THEN     set pedir = (stock_minimo+10)-v_stock; else  set pedir = NULL; END IF; SELECT pedir,v_cod;  END LOOP get_arts; CLOSE stock_art; END//
-
-3.- DELIMITER |
-CREATE PROCEDURE ejercicio3 ()  BEGIN  DECLARE v_edad INTEGER(10);  DECLARE v_nombre VARCHAR(10);  DECLARE v_fecha DATE;  DECLARE v_fin INTEGER DEFAULT 0;  DECLARE v_mostrar VARCHAR(10);  DECLARE cursor_edad CURSOR FOR SELECT nombre,fecha_nac FROM Cliente;  DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_fin=1;  OPEN cursor_edad;  get_edad: LOOP  FETCH cursor_edad INTO v_nombre,v_fecha;  SELECT TIMESTAMPDIFF(year,v_fecha,CURDATE()) INTO v_edad;  IF v_fin = 1 THEN  LEAVE get_edad;  END IF;  IF v_edad > 30 && v_edad < 60 THEN  SET v_mostrar = 'Adulto';  ELSEIF v_edad < 30 THEN  SET v_mostrar = 'Joven'; ELSE  SET v_mostrar = 'Jubilado'; END IF; SELECT v_nombre,v_mostrar,v_edad; END LOOP get_edad;  CLOSE cursor_edad;  END|
-4.- CREATE PROCEDURE cambiar_calle() BEGIN DECLARE v_DIRECCION VARCHAR(150); DECLARE v_DNI VARCHAR(30); DECLARE fin INTEGER DEFAULT 0; DECLARE  cliente_direccion CURSOR FOR   select DIRECCION,DNI from cliente ; DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin=1; OPEN cliente_direccion; get_client: LOOP  FETCH cliente_direccion INTO v_DIRECCION,v_DNI ; IF fin=1 THEN LEAVE get_client; END IF;  if v_DIRECCION = "Carril La Iglesia" THEN     UPDATE cliente SET direccion = "C/ La Iglesia" WHERE DNI = v_DNI;   elseif  v_DIRECCION = "C/ Almudena" THEN  UPDATE cliente SET direccion = "Avda Mediterráneo" WHERE DNI = v_DNI;   END IF; END LOOP get_client; CLOSE cliente_direccion; END//
- */
